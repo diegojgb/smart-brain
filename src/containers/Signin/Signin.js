6fb2a6e70a39e1be507a2682cc12a4983c2dee37
diegojgb <diegojose78@gmail.com>
2023-07-02 02:53:59 -0400

    fixed some spacing

--- src/containers/Signin/Signin.js
+++ src/containers/Signin/Signin.js
@@ -31,7 +31,6 @@ const Signin = ({ loadUser, onRouteChange }) => {
         })
     }
 
-  
     return (
         <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
             <main className="pa4 black-80">
